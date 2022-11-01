b8f77c3ef3e95c03a45773541a28ab4935ae99bc
Eduardo Costa <m4c0@users.noreply.github.com>
2022-11-01 19:13:19 +0000

    fix windows build

--- silog.windows.cpp
+++ silog.windows.cpp
@@ -1,9 +1,11 @@
-module silog;
+module;
 #include <exception>
 #include <filesystem>
 #include <fstream>
 #include <tchar.h>
 #include <windows.h>
+
+module silog;
 
 static auto exe_path() noexcept {
   TCHAR exepath[MAX_PATH + 1];
@@ -16,11 +18,10 @@ static auto exe_path() noexcept {
 }
 void silog::log(silog::log_level lvl, const char * msg) {
   std::time_t t = std::time(nullptr);
-  std::tm tm {};
-  localtime_s(&tm, &t);
+  std::tm * tm = std::localtime(&t);
 
   auto exe = exe_path();
   auto level = silog::impl::log_level_cstr(lvl);
   std::ofstream out { exe.replace_extension(".log"), std::ios::app };
-  out << std::put_time(&tm, "%F %T %z") << " [" << level << "] " << msg << std::endl;
+  out << std::put_time(tm, "%F %T %z") << " [" << level << "] " << msg << std::endl;
 }
