d0eeb5f6c7c01da7d7042c95adc669a96346b441
Eduardo Costa <m4c0@users.noreply.github.com>
2022-11-02 09:10:58 +0000

    ecow: objc code as a unit of mod

--- build.cpp
+++ build.cpp
@@ -5,19 +5,17 @@ using namespace ecow;
 int main() {
   seq all { "all" };
 
-  auto lib = all.add_unit<seq>("all objects");
-
-  auto m = lib->add_unit<mod>("silog");
+  auto m = all.add_unit<mod>("silog");
 
 #ifdef __APPLE__
   m->add_impl("silog.apple");
-  lib->add_unit("apple");
+  m->add_unit("apple");
 #elif _WIN32
   m->add_impl("silog.windows");
 #endif
 
   auto poc = all.add_unit<exe>("silog-poc");
-  poc->add_ref(lib);
+  poc->add_ref(m);
   poc->add_unit<>("poc");
 
   return all.build() ? 0 : 1;
