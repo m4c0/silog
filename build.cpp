852fdb3250a444895e185e4874de23c23f762016
Eduardo Costa <m4c0@users.noreply.github.com>
2022-11-07 12:50:53 +0000

    ecow: add android to the build

--- build.cpp
+++ build.cpp
@@ -11,10 +11,15 @@ int main(int argc, char ** argv) {
   m_steve.add_impl("silog.apple");
   m_steve.add_unit<objc>("apple.mm")->add_framework("Foundation");
 
-  m->for_feature(unit::feats::android_ndk).add_impl("silog.android");
+  auto & m_oreo = m->for_feature(unit::feats::android_ndk);
+  m_oreo.add_impl("silog.android");
+  // This may be a bug, but we don't need this using ecow (somehow)
+  // m_oreo.add_system_library("log");
+
   m->for_feature(unit::feats::windows_api).add_impl("silog.windows");
 
-  auto poc = all.add_unit<app>("silog-poc");
+  // Tests if we link properly. Can be run on Mac and Windows
+  auto poc = all.add_unit<exe>("silog-poc");
   poc->add_ref(m);
   poc->add_unit<>("poc");
 
