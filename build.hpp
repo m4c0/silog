db8ae035f2ce39db1d5c553d026878f8138847c6
Eduardo Costa <m4c0@users.noreply.github.com>
2023-08-15 14:29:55 +0100

    build: auto-discover module part

--- build.hpp
+++ build.hpp
@@ -16,24 +16,20 @@ auto silog() {
 })");
 
   auto & m_steve = m->for_feature(objective_c);
-  m_steve.add_part("base");
   m_steve.add_impl("silog.apple");
   m_steve.add_unit<objc>("apple");
   m_steve.add_impl("sprintf");
 
   auto & m_oreo = m->for_feature(android_ndk);
-  m_oreo.add_part("base");
   m_oreo.add_impl("silog.android");
   m_oreo.add_system_library("log");
   m_oreo.add_impl("sprintf");
 
   auto & m_wasm = m->for_feature(webassembly);
-  m_wasm.add_part("base");
   m_wasm.add_impl("silog.wasm");
   m_wasm.add_impl("sprintf");
 
   auto & m_bill = m->for_feature(windows_api);
-  m_bill.add_part("base");
   m_bill.add_impl("silog.windows");
   m_bill.add_impl("sprintf");
 
