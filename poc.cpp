1e9df35e073fc38fe393425da0dbadfaee7566e5
Eduardo Costa <m4c0@users.noreply.github.com>
2023-09-13 20:46:30 +0100

    poc: using a macro to avoid warnings on non-wasm

--- poc.cpp
+++ poc.cpp
@@ -1,6 +1,14 @@
+#pragma leco tool
+
 import silog;
 
-int __attribute__((export_name("main"))) main() {
+#if __wasm__
+#define WASM_MAIN __attribute__((export_name("main")))
+#else
+#define WASM_MAIN
+#endif
+
+int WASM_MAIN main() {
   silog::log(silog::info, "I'm %s", "ok");
   silog::assert(true, silog::debug, "I'm kinda ok");
   silog::assert(false, "I'm not ok");
