17aad8ad0a784f88b4031e77430b405f9b16ab60
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-02 10:31:25 +0100

    even better output on wasm

--- wasm.cpp
+++ wasm.cpp
@@ -5,5 +5,12 @@ module silog;
 
 void silog::impl::log(silog::log_level lvl, const char * msg) {
   auto level = silog::impl::log_level_cstr(lvl);
-  printf("[%s] %s\n", level, msg);
+  switch (lvl) {
+  case silog::error:
+    fprintf(stderr, "[%s] %s\n", level, msg);
+    break;
+  default:
+    printf("[%s] %s\n", level, msg);
+    break;
+  }
 }
