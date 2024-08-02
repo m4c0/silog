397e5018ad5d8e017906748c46b8278de3012f26
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-02 10:28:03 +0100

    working on c++ bits of WASM
    It is definitely simple if you know what you are doing AND you have the
    right tools

--- poc.cpp
+++ poc.cpp
@@ -1,14 +1,8 @@
-#pragma leco tool
+#pragma leco app
 
 import silog;
 
-#if __wasm__
-#define WASM_MAIN __attribute__((export_name("main")))
-#else
-#define WASM_MAIN
-#endif
-
-int WASM_MAIN main() {
+int main() {
   silog::log(silog::info, "I'm %s", "ok");
   silog::assert(true, silog::debug, "I'm kinda ok");
   silog::assert(false, "I'm not ok");
--- wasm.cpp
+++ wasm.cpp
@@ -1,14 +1,9 @@
 module;
 #include <stdio.h>
 
-extern "C" void __attribute__((import_name("silog_log"))) silog_log(const char * msg);
-
 module silog;
 
 void silog::impl::log(silog::log_level lvl, const char * msg) {
   auto level = silog::impl::log_level_cstr(lvl);
-
-  char buf[10240];
-  sprintf(buf, "[%s] %s", level, msg);
-  silog_log(buf);
+  printf("[%s] %s\n", level, msg);
 }
