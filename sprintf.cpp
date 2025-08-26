dae329f5415698408c0251af89428a96d8ea854e
Eduardo Costa <m4c0@users.noreply.github.com>
2025-08-26 18:46:11 +0100

    some wasm support

--- sprintf.cpp
+++ sprintf.cpp
@@ -2,6 +2,10 @@ module;
 #include <stdarg.h>
 #include <stdio.h>
 
+#ifdef LECO_TARGET_WASM
+#include <stdlib.h>
+#endif
+
 module silog;
 import :base;
 
@@ -30,7 +34,11 @@ void silog::die(const char * msg, ...) {
   va_end(arg);
 
   impl::log(error, buf);
+#ifndef LECO_TARGET_WASM
   throw death {};
+#else
+  abort();
+#endif
 }
 void silog::whilst(const char * msg, ...) {
   char buf[1024] = "while ";
@@ -41,5 +49,9 @@ void silog::whilst(const char * msg, ...) {
   va_end(arg);
 
   impl::log(error, buf);
-  throw;
+#ifndef LECO_TARGET_WASM
+  throw death {};
+#else
+  abort();
+#endif
 }
