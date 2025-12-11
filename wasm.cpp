2493e221cad78896d707d6a21620f2604fb567aa
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-11 13:26:04 +0000

    wasm: fix build

--- wasm.cpp
+++ wasm.cpp
@@ -1,14 +1,12 @@
-module;
-#include <stdio.h>
-
 module silog;
+import print;
 import vaselin;
 
 void silog::impl::log(silog::log_level lvl, sv msg) {
   auto level = silog::impl::log_level_cstr(lvl);
   if (lvl == silog::error) {
-    fprintf(stderr, "[%s] %s\n", level, msg);
+    errln("[", level, "] ", msg);
   } else {
-    printf("[%s] %s\n", level, msg);
+    putln("[", level, "] ", msg);
   }
 }
