4addd5b64d89e9300e180a09a2082df8103fce82
Eduardo Costa <m4c0@users.noreply.github.com>
2025-12-19 16:07:47 +0000

    apple: simpler stdout log when available
    Should reduce the terminal clutter

--- apple.cpp
+++ apple.cpp
@@ -6,6 +6,12 @@ extern "C" void NSLog(CFStringRef format, ...);
 module silog;
 
 void silog::impl::log(log_level lvl, sv msg) {
+  auto c = fprintf(stdout, "[%5s] %.*s\n",
+      impl::log_level_cstr(lvl),
+      static_cast<unsigned>(msg.size()),
+      msg.begin());
+  if (c > 0) return;
+
   NSLog(CFSTR("[%s] %.*s"),
       impl::log_level_cstr(lvl),
       static_cast<unsigned>(msg.size()),
